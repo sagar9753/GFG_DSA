b8197622f00698a0e0cc3297b5f1cb10e271f8cf
Sagar Rajput <sagar.rajput9753@gmail.com>
2023-10-28 23:30:55 +0530

    Create Bleak Numbers.java

---
+++ Bleak Numbers.java
@@ -0,0 +1,26 @@
+ques link - https://practice.geeksforgeeks.org/problems/bleak-numbers1552/1
+
+-------------------------------------- solution ----------------------------------------
+
+class Solution
+{
+    public int is_bleak(int n)
+    {
+        for(int x= n-1;x>=n-32;x--){
+            if(x+countSetBits(x) == n) 
+                    return 0;
+            }     
+        return 1;
+    }
+    
+    private int countSetBits(int num)
+    {
+        int count = 0;
+        while (num > 0)
+        {
+            count += num & 1;
+            num >>= 1;
+        }
+        return count;
+    }
+}
